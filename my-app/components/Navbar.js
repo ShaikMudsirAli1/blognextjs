3c966c4dd1eb30a6faa2a06beb8a6e405d314dd1
ShaikMudsirAli1 <121999550+SHAIKADS@users.noreply.github.com>
2023-04-27 00:22:38 +0530

    first commit

---
+++ my-app/components/Navbar.js
@@ -0,0 +1,57 @@
+import React from "react";
+import Script from "next/script";
+
+// Navbar component.
+// Jsx must have one parent element we should add react fragment <></>.
+const Navbar = () => {
+  return (
+    <>
+      <Script
+        src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/js/all.min.js"
+        integrity="sha256-KzZiKy0DWYsnwMF+X1DvQngQ2/FxF7MF3Ff72XcpuPs="
+        crossorigin="anonymous"
+      ></Script>
+      <nav className="w-full py-4 bg-blue-800 shadow">
+        <div className="w-full container mx-auto flex flex-wrap items-center justify-between">
+          <nav>
+            <ul className="flex items-center justify-between font-bold text-sm text-white uppercase no-underline">
+              <li>
+                <a
+                  className="hover:text-gray-200 hover:underline px-4"
+                  href="#"
+                >
+                  Shop
+                </a>
+              </li>
+              <li>
+                <a
+                  className="hover:text-gray-200 hover:underline px-4"
+                  href="#"
+                >
+                  About
+                </a>
+              </li>
+            </ul>
+          </nav>
+
+          <div className="flex items-center text-lg no-underline text-white pr-6">
+            <a className="" href="#">
+              <i className="fab fa-facebook"></i>
+            </a>
+            <a className="pl-6" href="#">
+              <i className="fab fa-instagram"></i>
+            </a>
+            <a className="pl-6" href="#">
+              <i className="fab fa-twitter"></i>
+            </a>
+            <a className="pl-6" href="#">
+              <i className="fab fa-linkedin"></i>
+            </a>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default Navbar;
---
+++ my-app/tailwind.config.js
@@ -0,0 +1,16 @@
+// Add the paths to all of your template files in your tailwind.config.js file.
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
